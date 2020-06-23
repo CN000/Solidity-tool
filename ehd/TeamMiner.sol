f576ba4f9f6ac02aa94a0dbd3f60d680c3c66c2f
Lucas <lukai.rat@gmail.com>
2020-06-23 18:19:11 +0800

    团队挖矿
    Took 4 hours 31 minutes

--- ehd/TeamMiner.sol
+++ ehd/TeamMiner.sol
@@ -1,83 +1,127 @@
 pragma solidity 0.4.8;
 
+contract MortageSystem {
+    function totalMortgage() public constant returns (uint256) {}
+    function mortgageOf(address owner) public constant returns (uint256) {}
+}
+
+// 0x0000000000000000000000000000000000000082
 contract TeamMiner {
-    address public mortgageAddress = 0x0000000000000000000000000000000000000081;
+    MortageSystem mortageContract;
 
-    uint maxReward = 167;
+    // 质押合约地址
+    address public mortgageAddress     = 0x0000000000000000000000000000000000000081;
 
-    // 最低质押EHD数目
-    uint256 private minMortgagedAmount = 100;
+    // 基金会地址
+    address public fundAddress         = 0x246229BF5719c05133d5f52De39817BceB829a06;
+
+    // 最大区块奖励
+    uint256 public maxBlockReward      = 168*(10**18);
+
+    // 最少质押数目
+    uint256 private minMortgagedAmount = 1000;
+
+    // Miner Address List
+    address[] public minerList;
 
     struct Block {
-        uint256   blockNumber;
-        address   miner;
-        uint256   minerMortgagedAmount;
-        address[] minerList;
-        bool      isRewardProcessed;
+       uint256   blockNumber;
+       address   miner;
+       uint256   minerMortgagedAmount;
+       address[] minerList;
+       bool      isRewardProcessed;
     }
 
     // Index of a person is its ID.
     Block[] blocks;
 
-    // Miner Address List
-    mapping(address => bool) private minerList;
+    // constructor () public {}
 
-    // Support to Receive EHD
-    constructor() public payable {
-        // 自动检查规范的矿工列表 minerList 决定移除列表
+    function () external payable{ }
+
+    function clean() public returns(bool) {
+        // Automatic Remove Miner
+        for(uint256 i=0; i<minerList.length; i++) {
+            if( getMortgageAmount(minerList[i]) < minMortgagedAmount) {
+                delete minerList[i];
+            }
+        }
+
+        return true;
     }
 
     // register address to miner list
-    function register(address miner) public returns (bool) {
-        // 检查质押额度  达到  10？？？EHD , 才可以加入合作挖矿队列
-        if(!_isAllowedToRegister(account)) {
-            minerList[account] = true;
-        }
-
-        // TODO
+    function register(address account) public returns (bool) {
+        minerList.push(account);
         return true;
     }
 
-    //remove address from miner list
-    //  https://ethereum.stackexchange.com/questions/56281/how-to-check-if-an-array-key-exists
-    function unregister(address miner) public returns (bool) {
-        if(1){ }
-        return true;
+    // remove address from miner list
+    // https://ethereum.stackexchange.com/questions/56281/how-to-check-if-an-array-key-exists
+    function unregister(address account) public returns (bool) {
+       for(uint256 i=0; i < minerList.length; i++) {
+           if( minerList[i] == account ) {
+               delete minerList[i];
+           }
+       }
+
+       return true;
     }
 
-    // 上传矿机的相关信息
-    function upload(uint256 blockNumber, address miner, string ip, uint256 totalDiskSpace, uint256 totalPlotedDiskSpace, uint256 mortgageAmount) public returns(bool) {
-        // TODO
-        return true;
-    }
-
-    //  检查是否允许注册为矿机商
-    function _isAllowedToRegister(address account) payable public returns (bool) {
-        // TODO
-        return true;
-    }
-
-    // Total Mortgage Amount in System
+    // 获取质押合约总数
     function getTotalMortgageAmount() public returns (uint256) {
-        // TODO
-        return 1415;
+        mortageContract = MortageSystem(mortgageAddress);
+        return mortageContract.totalMortgage();
+        // return mortgageAddress.call(bytes4(keccak256("totalMortgage()")));
     }
 
+    // 获取质押数量
     function getMortgageAmount(address account) public returns (uint256) {
-        // TODO
-        return 1314;
+        mortageContract = MortageSystem(mortgageAddress);
+        return mortageContract.mortgageOf(account);
+
+        // return mortgageAddress.call(bytes4(keccak256("mortgageOf(address)")), account);
     }
 
-    // https://ethereum.stackexchange.com/questions/17312/solidity-can-you-return-dynamic-arrays-in-a-function
-    function getMinerList() public returns () {
-        return minerList;
-    }
+    // 处理每个区块奖励
+    function processReward() public returns (bool) {
+        address[] memory _minerList;
+        address   miner;
+        uint256   _fundReward;
+        uint256   _avgRewardPerMiner;
 
-    function processReward()
-    {
-        for(uint256 i =0; i <= blocks.length; i++) {
-            if(blocks.)
 
+        for(uint256 i=0; i<=blocks.length; i++) {
+            if( blocks[i].isRewardProcessed == true ) {
+               continue;
+            }
+
+            _fundReward = maxBlockReward/5;  // 基金会 20%
+
+            miner       = blocks[i].miner;
+
+            // 合作挖矿平分奖励
+            _minerList         = blocks[i].minerList;
+            _avgRewardPerMiner = ((maxBlockReward*2/5)/_minerList.length); // 平均合作挖矿奖励   40%
+            for(uint256 j=0; j<_minerList.length; j++) {
+               _minerList[j].send(_avgRewardPerMiner);
+            }
+
+            // 基金会地址
+            //require(fundAddress.send(_fundReward), "Send FundReward");
+            if (!fundAddress.send(_fundReward)){
+                throw;
+            }
+
+            // 这个区块奖励已经处理结束
+            blocks[i].isRewardProcessed = true;
         }
+
+        return true;
+    }
+
+    // 获取矿工列表
+    function getMinerList()  public returns (address[] memory){
+        return minerList;
     }
 }
